05e63b7693ab541901ddd18e50e16234b2a7e21f
Romaric Paul <romaric.paul@businessdecision.com>
2018-03-27 17:40:06 +0200

    simplify scroll to be less tricky

--- assets/jquery.fullpage.js
+++ assets/jquery.fullpage.js
@@ -2,94 +2,24 @@ $(document).ready(function() {
     if($(".home:not(.documentation)").length) {
         var section = $('.section1');
         var lastScrollTop = 0;
-        var animating = false;
-        var height2 = $(window).height() * 2;
 
         $(".btnDiscover").on("click", function(e){
             e.preventDefault();
-            if(!animating) {
-                animating = true;
-                $("html, body").animate({
-                    scrollTop: ($(".container_advantages").offset().top - $(window).height()),
-                    easing: 'linear'
-                }, 1000, function() {
-                    setTimeout(function(){ animating = false; }, 500);
-                    var st = st = $(document).scrollTop();
-                    lastScrollTop = st;
-                });
-                section = section.next();
-            }
+            $("html, body").animate({
+                scrollTop: ($(".container_advantages").offset().top - $(window).height()),
+                easing: 'linear'
+            }, 500);
+            section = section.next();
         });
 
         $(".btnFeatures").on("click", function(e){
             e.preventDefault();
             scrollable = false;
-            if(!animating) {
-                animating = true;
-                $("html, body").animate({
-                    scrollTop: $(".container_advantages").offset().top,
-                    easing: 'linear'
-                }, 700, function() {
-                    scrollable = true;
-                    setTimeout(function(){ animating = false; }, 700);
-                    var st = st = $(document).scrollTop();
-                    lastScrollTop = st;
-                });
-                section = section.next();
-            }
-        });
-
-        $(window).scroll(function(event) {
-            if($('html').is(':animated') || $('body').is(':animated')) {
-                return false;
-            }
-
-            var st = $(this).scrollTop();
-
-            if (st > lastScrollTop){
-                // downscroll code
-                if(!animating) {
-                    section = section.next();
-                    animating = true;
-                    if(st > height2) {
-                        animating = false;
-                        section = section.prev();
-                    } else {
-                        console.log("bottom");
-                        if (section.length) {
-                            $("html, body").animate({
-                                scrollTop: section.offset().top,
-                                easing: 'default'
-                            }, 700, function() {
-                                setTimeout(function(){ animating = false; }, 400);
-                            });
-                        }
-                    }
-                }
-            } else {
-                // upscroll code
-                if(!animating) {
-                    section = section.prev();
-                    animating = true;
-                    if(st > height2) {
-                        animating = false;
-                        section = section.next();
-                    } else {
-                        console.log("top");
-                        if (section.length) {
-                            $("html, body").animate({
-                                scrollTop: section.offset().top,
-                                easing: 'default'
-                            }, 700, function() {
-                                setTimeout(function(){ animating = false; }, 400);
-                            });
-                        }
-                    }
-                }
-            }
-
-            st = $(document).scrollTop();
-            lastScrollTop = section.offset().top;
+            $("html, body").animate({
+                scrollTop: $(".container_advantages").offset().top,
+                easing: 'linear'
+            }, 500);
+            section = section.next();
         });
     }
 });
